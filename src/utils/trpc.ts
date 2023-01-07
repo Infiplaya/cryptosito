b63b0b135c1ad8ed7daf5ec7287fb956a197913d
Pawel Sobaniec <infipablosoban@gmail.com>
2023-01-07 21:59:17 +0100

    enable refetch on mount

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -17,7 +17,6 @@ export const trpc = createTRPCNext<AppRouter>({
       queryClientConfig: {
         defaultOptions: {
           queries: {
-            refetchOnMount: false,
             refetchOnWindowFocus: false,
           }
         }
